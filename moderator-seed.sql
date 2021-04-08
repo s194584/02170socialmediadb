cc6a2d471d75506d8c4b81944b19a1eee03baa49
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2021-04-08 09:40:26 +0200

    Add moderator seed

---
+++ moderator-seed.sql
@@ -0,0 +1,7 @@
+INSERT INTO moderators VALUES
+(1, 100000, "John Doe"),
+(2, 105000, "Jane Doe"),
+(3, 65000, " Johnnie Jannine"),
+(4, 78000, "Wyatt Marsh"),
+(5, 35000, "Hareem Ritter"),
+(6, 82000, "Jon Duggan");

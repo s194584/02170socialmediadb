9aabef98b610c33f08f0f9171221c081a4ef95de
KristineKlok <55542051+KristineKlok@users.noreply.github.com>
2021-04-08 11:43:05 +0200

    Same but prettier

--- SEED.sql
+++ SEED.sql
@@ -58,15 +58,15 @@ INSERT Followers VALUES
 
 # Filling the communities table
 INSERT INTO communities VALUES(1, "Software2019", current_timestamp(), true, 1, 1),
-								(2, "Buy & Sell - Lyngby", current_timestamp(), true, 2, 2),
-                                (3, "Mat 1 memes", current_timestamp(), true, 3, 3),
-                                (4, "The Sørensen Family", current_timestamp(), false, 4, 4),
-                                (5, "Internal group for POP dormitory", current_timestamp(), false, 5, 5),
-                                (6, "Fishing enthusiasts", current_timestamp(), true, 6, 6),
-                                (7, "Vegetarian Receipes", current_timestamp(), true, 7, 1),
-                                (8, "Fan group for ThatOneSportsDude", current_timestamp(), true, 8, 2),
-                                (9, "Copenhagen wine club", current_timestamp(), true, 9, 3),
-                                (10, "TheOneEvent", current_timestamp(), false, 10, 4);
+(2, "Buy & Sell - Lyngby", current_timestamp(), true, 2, 2),
+(3, "Mat 1 memes", current_timestamp(), true, 3, 3),
+(4, "The Sørensen Family", current_timestamp(), false, 4, 4),
+(5, "Internal group for POP dormitory", current_timestamp(), false, 5, 5),
+(6, "Fishing enthusiasts", current_timestamp(), true, 6, 6),
+(7, "Vegetarian Receipes", current_timestamp(), true, 7, 1),
+(8, "Fan group for ThatOneSportsDude", current_timestamp(), true, 8, 2),
+(9, "Copenhagen wine club", current_timestamp(), true, 9, 3),
+(10, "TheOneEvent", current_timestamp(), false, 10, 4);
                                 
 # Happenings
 -- Inspiration found from meetup.com

67873a639816f85f2e1782e237d277588b3f7541
KristineKlok <55542051+KristineKlok@users.noreply.github.com>
2021-04-12 14:13:31 +0200

    Collected files into socialmediadb

--- socialmediadb.sql
+++ socialmediadb.sql
@@ -238,8 +238,7 @@ INSERT INTO stories VALUES
 (7, "https://sociallysorted.com.au/wp-content/uploads/2020/01/Remove-Background-Image-3-Toolsto-Nail-It-One-Click.jpg", 5, NOW()),
 (8, "https://theinpaint.com/images/example-1-2.jpg", 2, NOW()),
 (9, "https://justifiedgrid.com/wp-content/uploads/life/biking/137646854.jpg", 1, NOW()),
-(10, "https://res.cloudinary.com/demo/image/upload/w_500,f_auto/sample.jpg", 8, NOW()),               
-(11, "This should be deleted after the 10 minutes", 1, NOW() - interval 24 HOUR);
+(10, "https://res.cloudinary.com/demo/image/upload/w_500,f_auto/sample.jpg", 8, NOW());
 
 INSERT INTO posts VALUES
 
@@ -373,6 +372,7 @@ DROP VIEW IF EXISTS postPGRating;
 DROP VIEW IF EXISTS communityPGRating;
 DROP VIEW IF EXISTS publicPosts;
 DROP VIEW IF EXISTS publicGroups;
+DROP VIEW IF EXISTS memberContactInfos;
 
 # Create views
 CREATE VIEW thisWeeksHappenings(happenID, startTime, endTime) as
@@ -406,14 +406,46 @@ CREATE VIEW publicPosts AS SELECT userName, postText, postImage, postedAt FROM (
 
 CREATE VIEW publicGroups AS SELECT communityName FROM communities WHERE isPublic = true;
 
+CREATE VIEW memberContactInfos as
+SELECT c.communityID,c.communityName,u.userName,u.email 
+FROM members as m JOIN users as u JOIN communities as c 
+ON m.userID=u.userID AND m.communityID=c.communityID;
+
 
 # Event ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 set global event_scheduler = on;
 
-CREATE EVENT removeStories ON SCHEDULE EVERY 10 MINUTE DO 
+CREATE EVENT removeStories ON SCHEDULE EVERY 5 SECOND DO 
 	DELETE FROM stories WHERE (NOW() - interval 24 HOUR) > publishedAt;
+
+# Function to determine whether or not the passed user can see the passed post -----------------------------------------------------------------------------------------------------------------------------------
+-- PGRating, no associated group or membership in the group.
+DROP FUNCTION IF EXISTS canSeePost;
+DELIMITER //
+CREATE FUNCTION canSeePost(uID INT, pID INT)
+RETURNS boolean
+BEGIN
+	DECLARE age INT;
+    DECLARE comID INT;
+    DECLARE underagedTags INT;
+    DECLARE memberCom INT;
     
-    
+    SELECT TIMESTAMPDIFF(year,dateOfBirth,now()) INTO age FROM users WHERE userID=uID;
+    # Find and count the underaged tags
+    SELECT COUNT(*) INTO underagedTags FROM tags WHERE tag IN (SELECT tag FROM tagged WHERE postID=pID) AND age<PGRating;
+    # If any underaged tags are counted, then the user is underage.
+    IF underagedTags != 0 THEN RETURN false; END IF;
+    # See if whether the thingy is public or is in a group
+    SELECT communityID INTO comID FROM posts WHERE postID=pID;
+    IF comID is NULL THEN RETURN TRUE; END IF;
+    # Then the post is part of a community, but is the user part of it
+    SELECT COUNT(*) INTO memberCom FROM members WHERE userID=uID AND communityID=comID;
+    IF memberCom = 0 THEN RETURN FALSE; END IF;
+     
+    RETURN TRUE;
+END; //
+DELIMITER ;
+
 # Query to get user activity --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 ((SELECT userID, postedAt AS activity FROM posts)
 UNION ALL
@@ -421,14 +453,17 @@ UNION ALL
 UNION ALL
 (SELECT userID, publishedAt FROM stories) ORDER BY userID;
 
-# Get all posts by user -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Query to find the most popular tags -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+SELECT tag,COUNT(*) as count FROM tagged GROUP BY tag HAVING COUNT(*) > 2 ORDER BY count DESC;
+
+# Query - Get all posts by user -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 SELECT posts.postID, posts.postText, posts.postImage, posts.postedAt, users.userName, communities.communityName
 FROM posts 
 INNER JOIN users ON posts.userID = users.userID
 INNER JOIN communities ON posts.communityID = communities.communityID
 WHERE posts.userID = 9;
 
-# Updating tag to PGRating 13 ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Updating tag to PGRating 13 ... (SHOWING UPDATE) ----------------------------------------------------------------------------------------------------------------------------------------------------------------
 SET SQL_SAFE_UPDATES = 0;
 
 SELECT * FROM tags;
@@ -443,7 +478,8 @@ THEN 13
 ELSE PGRating
 END;
 
-# Deleting all users who are younger than 13 ---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# Deleting all users who are younger than 13 ... (SHOWING DELETE) -------------------------------------------------------------------------------------------------------------------------------------------------
+DROP FUNCTION IF EXISTS age;
 CREATE FUNCTION Age (vDate DATE) RETURNS INTEGER
 RETURN TIMESTAMPDIFF(YEAR, vDate , CURDATE());
 
@@ -455,3 +491,29 @@ Where userID = 4 or userID = 6;
 
 DELETE FROM users
 WHERE Age(dateOfBirth) < (SELECT min(PGRating) FROM tags);
+
+
+
+
+
+
+# TESTS ---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+## VIEWS
+# SELECT * FROM thisWeeksHappenings;
+# SELECT * FROM communitiesWithOwner;
+# SELECT * FROM communityMemberAmount;
+# SELECT * FROM postPGRating;
+# SELECT * FROM communityPGRating;
+# SELECT * FROM publicPosts;
+# SELECT * FROM publicGroups;
+# SELECT * FROM memberContactInfos;
+
+## FUNCTIONS
+SELECT * FROM posts WHERE canSeePost(6,postID);  # find all posts that user with ID 6 can see.
+
+## EVENT - Show that storie from 24 hours ago is deleted.
+INSERT INTO stories VALUES(11, "This should be deleted after the 5 SECONDS", 1, NOW() - interval 24 HOUR);
+SELECT * FROM stories;
+DO SLEEP(5);
+SELECT * FROM stories;
